f5b128145065125ebcecdcff9ddc476b5fbd321a
Dan Chambers <danlechambre@icloud.com>
2023-03-17 00:57:13 +0000

    Update types for fave item

--- src/components/Faves/FaveItem.tsx
+++ src/components/Faves/FaveItem.tsx
@@ -1,14 +1,15 @@
- import { Button,   List, Rating, } from 'semantic-ui-react'
+import { Button, List, Rating } from 'semantic-ui-react'
+import { FaveItemObj, handleRating, handleRemove } from './types'
 
- interface Props{
-	 fave:any;
-	 handleRating:any;
-	 handleRemove:any;
- }
+interface Props {
+	fave: FaveItemObj
+	handleRating: handleRating
+	handleRemove: handleRemove
+}
 
-const FaveItem = ({ fave, handleRating, handleRemove }:Props)=> (
+const FaveItem = ({ fave, handleRating, handleRemove }: Props) => (
 	<List.Item>
-		<img className='ui mini circular image' src="avatar.jpg" alt="avatar" />
+		<img className="ui mini circular image" src="avatar.jpg" alt="avatar" />
 		<List.Content>
 			{fave.name} <Button onClick={handleRemove} size="mini" icon={{ name: 'delete', color: 'red' }} basic />
 		</List.Content>
--- src/components/Faves/types.ts
+++ src/components/Faves/types.ts
@@ -1,7 +1,9 @@
-import {SyntheticEvent} from 'react'
-import {ButtonProps, RatingProps} from 'semantic-ui-react'
-export interface FaveItemObj{
-    id:number;
+import { SyntheticEvent } from 'react'
+import { ButtonProps, RatingProps } from 'semantic-ui-react'
+export interface FaveItemObj {
+	id: number
+	name: string
+	rating: string
 }
 export type handleRemove = (_e: SyntheticEvent, data: ButtonProps) => void
 export type handleRating = (_e: SyntheticEvent, data: RatingProps) => void
